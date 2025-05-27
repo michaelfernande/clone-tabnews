1ccf3601113d0a5bc249c9d9b6cbbe23d0cc11cc
michaelfernande <michaelfernande@hotmail.com>
2025-05-27 18:54:54 -0300

    adicionar scripts `lint:check e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home () {
-    return <h1>sei que temos que mudar mas eu tbm tenho medo pois não tenho como te ajudar financeiramente no momento</h1>
+function Home() {
+  return (
+    <h1>
+      sei que temos que mudar mas eu tbm tenho medo pois não tenho como te
+      ajudar financeiramente no momento
+    </h1>
+  );
 }
 
-export default Home; 
+function teste() {
+  console.log("teste");
+}
+
+export default Home;
