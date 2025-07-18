172d8dc0a09835c76ba2fa33b7594bf36c7d2bc1
michaelfernande <michaelfernande@hotmail.com>
2025-07-18 10:51:05 -0300

    fix  andpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,7 +3,17 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)){
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
+}
+
+  let dbClient;
+
+  try {
+  dbClient = await database.getNewClient();
 
   const defaultMigrationOptions = {
     dbClient: dbClient,
@@ -16,7 +26,6 @@ export default async function migrations(request, response) {
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
 
@@ -26,7 +35,6 @@ export default async function migrations(request, response) {
       dryRun: false,
     });
 
-    await dbClient.end();
 
     if (migratedMigrations.length > 0) {
       return response.status(201).json([migratedMigrations]);
@@ -35,5 +43,10 @@ export default async function migrations(request, response) {
     return response.status(200).json(migratedMigrations);
   }
 
-  return response.status(405).end();
+} catch (error){
+  console.error(error);
+  throw error;
+} finally {
+  await dbClient.end();
+}
 }
